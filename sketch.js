f260d9f91bdce43be266f23ee3cefafe1b552134
Sydnie Lei <johndoe@example.com>
2023-11-06 15:32:38 +1100

    Add the function of mousePressed

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let circles = [];
 let yOffset = 0;
+let performAnimation = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -25,15 +26,45 @@ function draw() {
         interpColor = lerpColor(color(209,134,61), color(88,142,189), map(d, 200.1, 500, 0, 1));
       }
       stroke(interpColor);
-
-      //Add the effect of Ripple Diffusion to mimic the flow of river
-      let xOffset = sin(frameCount * 5 + i * 10 + j * 2) * 10;
-      vertex(x + xOffset, y);
-      
+      strokeWeight(2);
+      vertex(x, y);
       noFill();
     }
     endShape(CLOSE);
   }
+
+
+  for (let circle of circles) {
+    fill(circle.color);
+    noStroke();
+    ellipse(circle.x, circle.y, circle.radius * 2);
+  }
+
+  if (performAnimation) {
+    for (let i = 0; i < 100; i++) {
+      beginShape();
+      for (let j = 0; j < 360; j += 10) {
+        let rad = i * 5;
+        let x = rad * cos(j);
+        let y = rad * sin(j);
+        let d = dist(0, 0, x, y);
+        let interpColor;
+        if (d <= 200) {
+          interpColor = lerpColor(color(196, 99, 85), color(220, 147, 47), map(d, 0, 200, 0, 1));
+        } else {
+          interpColor = lerpColor(color(220, 147, 47), color(69, 106, 162), map(d, 200.1, 500, 0, 1));
+        }
+        stroke(interpColor);
+        strokeWeight(2);
+        let xOffset = sin(frameCount * 5 + i * 10 + j * 2) * 10;
+        vertex(x + xOffset, y);
+        noFill();
+      }
+      endShape(CLOSE);
+    }
+  }
+
+
 
   translate(0, 0, 100);
   rotateX(7);
@@ -58,6 +89,18 @@ function draw() {
     }
     endShape(CLOSE);
   }
+}
+
+//set the mousepressed
+function mousePressed() {
+  let newCircle = {
+    x: mouseX,
+    y: mouseY,
+    radius: random(20, 50),
+    color: color(random(255), random(255), random(255))
+  };
+  circles.push(newCircle);
+  performAnimation = true;
 }
 
 function windowResized() {
